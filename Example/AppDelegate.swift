87a737a18387633231376c2eb16fcb937d214b24
Alex Littlejohn <littlejohn@blazingchilli.com>
2015-07-01 14:19:49 +0200

    Added example project

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ALCameraViewController
+//
+//  Created by Alex Littlejohn on 2015/06/17.
+//  Copyright (c) 2015 zero. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = ALCameraViewController(croppingEnabled: true, completion: { (image) -> Void in
+            
+        })
+        window!.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ALCameraViewController
+//
+//  Created by Alex Littlejohn on 2015/06/17.
+//  Copyright (c) 2015 zero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
