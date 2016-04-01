69b6f5aa9c937dc76711f354d39cd89015806b20
Alex Littlejohn <alexlittlejohn@me.com>
2016-04-01 15:54:10 +0200

    Shared framework scheme

---
+++ CameraViewController/CameraViewController.h
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.h
+//  CameraViewController
+//
+//  Created by Alex Littlejohn on 2016/04/01.
+//  Copyright © 2016 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraViewController.
+FOUNDATION_EXPORT double CameraViewControllerVersionNumber;
+
+//! Project version string for CameraViewController.
+FOUNDATION_EXPORT const unsigned char CameraViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraViewController/PublicHeader.h>
+
+
