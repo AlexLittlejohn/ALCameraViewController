7340059a0d20c3444be4e11026706bf86826ab42
Geert Michiels <gm@gemic.net>
2021-01-31 08:36:09 +0100

    added CameraView.strings as resource in package

--- Package.swift
+++ Package.swift
@@ -23,6 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ALCameraViewController",
-            dependencies: [])
+            dependencies: [],
+            resources: [
+                .process("CameraView.strings")
+            ])
     ]
 )
